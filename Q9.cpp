1d78365507b62f0a98ec82fd8ddd0ea989ce4068
Dayasagar Khongbantabam <dayasagar.khon2024@lpu.in>
2025-02-04 00:53:47 +0530

    Update Q9.cpp

--- Q9.cpp
+++ Q9.cpp
@@ -1,10 +1,9 @@
-// a program to implement a Direct Address Table for integer keys ranging from 0 to n-1
+//a program to implement a Direct Address Table for integer keys ranging from 0 to n-1
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-class DirectAddressTable
-{
+class DirectAddressTable {
 private:
     vector<int> table;
     vector<bool> isOccupied;
@@ -12,61 +11,61 @@ private:
 public:
     DirectAddressTable(int size) : table(size, 0), isOccupied(size, false) {}
 
-    void insert(int key, int value)
-    {
-        if (key >= 0 && key < table.size())
-        {
+    void insert(int key, int value) {
+        if (key >= 0 && key < table.size()) {
             table[key] = value;
             isOccupied[key] = true;
         }
     }
 
-    string search(int key)
-    {
-        if (key >= 0 && key < table.size() && isOccupied[key])
-        {
-            return to_string(table[key]);
+    string search(int key) {
+        if (key >= 0 && key < table.size() && isOccupied[key]) {
+            return  to_string(table[key]);
         }
         return "NOT FOUND";
     }
 
-    void remove(int key)
-    {
-        if (key >= 0 && key < table.size())
-        {
+    void remove(int key) {
+        if (key >= 0 && key < table.size()) {
             isOccupied[key] = false;
         }
     }
 };
 
-int main()
-{
+int main() {
     int n, q;
     cin >> n >> q;
 
     DirectAddressTable dat(n);
 
-    for (int i = 0; i < q; i++)
-    {
+    for (int i = 0; i < q; i++) {
         string operation;
         int key;
         cin >> operation >> key;
 
-        if (operation == "INSERT")
-        {
+        if (operation == "INSERT") {
             int value;
             cin >> value;
             dat.insert(key, value);
-        }
-        else if (operation == "SEARCH")
-        {
-            cout << dat.search(key) << endl;
-        }
-        else if (operation == "DELETE")
-        {
+        } else if (operation == "SEARCH") {
+            cout << dat.search(key) <<  endl;
+        } else if (operation == "DELETE") {
             dat.remove(key);
         }
     }
 
     return 0;
 }
+
+
+//Output:
+// 5
+// 6
+// INSERT 2 100
+// INSERT 3 200
+// SEARCH 2
+// 100
+// DELETE 2
+// SEARCH 2
+// NOT FOUND
+// SEARCH 3
